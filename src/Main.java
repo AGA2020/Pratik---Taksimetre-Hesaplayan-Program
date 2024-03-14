15b363b79e0b3872fc0472a5089d64012f333227
AGA2020 <hasan9u@gmail.com>
2024-03-14 19:02:41 +0300

    Project completed

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+            double a,b;
+        Scanner girdi = new Scanner(System.in);
+        System.out.print("Kaç km gidildi : " );
+        a = girdi.nextDouble();
+        b = 10.0 + (a * 2.20);
+        b = (b<20) ? 20 : b;
+        System.out.print("Ücret : " + b);
     }
 }
